cf9cc030d6a6ed5d9a384738b04c7cf4c1668f2c
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-30 16:15:39 +0500

    Add last Home Work!

---
+++ task66/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму 
+натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+
+int sum = 0;
+
+for (int i = M; i <= N; i++)
+    sum += i;
+
+
+Console.WriteLine($"Сумма чисел равна: {sum}");
---
+++ task68/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+int AckermanFun(int n, int m)
+{
+    while (n != 0)
+    {
+        if (m == 0){m = 1;}
+        else{m = AckermanFun(n, m - 1);}
+        n = n -1;
+    }
+    return m + 1;
+}
+Console.WriteLine(AckermanFun(m, n));
